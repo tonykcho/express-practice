2b2c98accb46bc6629268d045aab7b22d00d029e
tony <tonykcho3527657@gmail.com>
2022-03-20 15:29:48 -0400

    winston logging

--- src/logger.ts
+++ src/logger.ts
@@ -1,14 +1,25 @@
 import log4js from "log4js";
+import winston from "winston";
+// import ecsFormat from "@elastic/ecs-winston-format";
 
 module LoggerModule {
-    log4js.configure({
-        appenders: { 'file': { type: 'file', filename: `logs/file.log` } },
-        categories: { default: { appenders: ['file'], level: 'debug' } }
-    })
 
-    export const logger = log4js.getLogger();
+    export const logger = winston.createLogger({
+        level: 'debug',
+        // format: ecsFormat(),
+        transports: [
+            new winston.transports.Console({ format: winston.format.simple() }),
+            new winston.transports.File({ filename: `logs/file.log`, format: winston.format.simple() })
+        ]
+    });
+    // log4js.configure({
+    //     appenders: { 'file': { type: 'file', filename: `logs/file.log` } },
+    //     categories: { default: { appenders: ['file'], level: 'debug' } }
+    // })
 
-    logger.level = "debug";
+    // export const logger = log4js.getLogger();
+
+    // logger.level = "debug";
 }
 
 export const logger = LoggerModule.logger;
