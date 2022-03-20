fbf91eb5491e9e9b32f42c6b8942bc8efae1bf97
tony <tonykcho3527657@gmail.com>
2022-03-20 14:58:47 -0400

    k8s & logging to file

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,11 @@
 import log4js from "log4js";
 
 module LoggerModule {
+    log4js.configure({
+        appenders: { 'file': { type: 'file', filename: `logs/file.log` } },
+        categories: { default: { appenders: ['file'], level: 'debug' } }
+    })
+
     export const logger = log4js.getLogger();
 
     logger.level = "debug";
