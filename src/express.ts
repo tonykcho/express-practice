4ea076e148fe8ae08464c14ee490446cac0b81e7
tony <tonykcho3527657@gmail.com>
2022-03-20 14:01:23 -0400

    Update express.ts

--- src/express.ts
+++ src/express.ts
@@ -6,9 +6,29 @@ export module ExpressModule {
 
     const port = 3000;
 
+    app.use(express.json());
+
+    var numbers: number[] = [];
+
     app.get('/', (req, res) => {
-        res.send("Hello World!");
+        res.send(numbers);
     });
+
+    app.post('/', (req, res) => {
+        const body: { id: number, text: string } = req.body;
+
+        logger.info(body);
+
+        if (body.id == null) {
+            res.statusCode = 400;
+            res.send("Bad Request!");
+            return;
+        }
+
+        numbers.push(body.id);
+
+        res.send("Got a Post request");
+    })
 
     export function start() {
         app.listen(port, () => {
